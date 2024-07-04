47dad930a6f3016a215eb33b6423540918c31e08
Luis Antonio Castro <luisantoniokoyari@gmail.com>
2024-07-04 15:08:23 +0200

    components

--- spiral/app/page.tsx
+++ spiral/app/page.tsx
@@ -1,59 +1,26 @@
-import Image from "next/image";
+import ButtonMain from "@/components/Button";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <main>
-      <div className="fixed left-0 right-0 top-0 bg-white p-2 pl-6   border-cyan-500 flex items-center">
-        <button className="mr-4">
-          <Image alt="menu" src="/menu.svg" width={30} height={30} />
-        </button>
-        <button className=" text-2xl font-extrabold text-cyan-900 hover:text-cyan-600 transition duration-500">
-          spiral
-        </button>
-        <button className="font-extralight text-2xl text-cyan-700 hover:text-cyan-600 tracking-wider hover:tracking-widest transition-all duration-1000 ease-out">
-          harmony
-        </button>
-        <button className="ml-auto mr-6 rounded-full bg-indigo-500 hover:bg-indigo-600 transition duration-500 ease-in-out text-white p-2 pl-3 pr-3 shadow-md">
-          LA
-        </button>
+    <main className="bg-cyan-50">
+      <Header />
+      <div className="mt-12 p-6 ml-32">
+        <h1 className="font-bold text-6xl text-indigo-600 mt-52">
+          Spiral Harmony
+        </h1>
+        <h2 className="mt-6 text-3xl text-slate-500">
+          Next generation Music Creation & Education
+        </h2>
+        <h3 className="mt-6 text-2xl  tracking-wide">
+          Are you ready to dive into the magical word of music and sound?
+        </h3>
       </div>
-      <div className="mt-12 flex flex-wrap p-6">
-        <button className="min-w-80 rounded-lg p-6 text-2xl bg-cyan-500 hover:bg-cyan-600 active:bg-slate-300  transition duration-1000 ease-in-out text-white m-3 shadow-xl flex flex-col items-center justify-center  hover:scale-105">
-          Play
-          <Image
-            alt="play"
-            src="/play.svg"
-            width={80}
-            height={80}
-            className="mt-5"
-          />
-        </button>
-        <button className="min-w-80 rounded-lg p-6 text-2xl bg-cyan-500 hover:bg-cyan-600 active:bg-slate-300  transition duration-1000 ease-in-out text-white m-3 shadow-xl flex flex-col items-center justify-center hover:scale-105">
-          Practice
-          <Image
-            alt="guitar"
-            src="/guitar.svg"
-            width={80}
-            height={80}
-            className="mt-5"
-          />
-        </button>
-        <button className="min-w-80 rounded-lg p-6 text-2xl bg-cyan-500 hover:bg-cyan-600 active:bg-slate-300  transition duration-1000 ease-in-out text-white m-3 shadow-xl flex flex-col items-center justify-center hover:scale-105">
-          Learn
-          <Image
-            alt="piano"
-            src="/piano.svg"
-            width={80}
-            height={80}
-            className="mt-5"
-          />
-        </button>
-        <button className="min-w-80 rounded-lg p-6 text-2xl bg-cyan-500 hover:bg-cyan-600 active:bg-slate-300  transition duration-1000 ease-in-out text-white m-3 shadow-xl flex flex-col items-center justify-center hover:scale-105">
-          yeah
-        </button>
-        <button className="min-w-80 rounded-lg p-6 text-2xl bg-indigo-500 hover:bg-indigo-600 active:bg-slate-300 transition duration-1000 ease-in-out text-white m-3 shadow-xl flex flex-col items-center justify-center hover:scale-105">
-          yeah
-        </button>
+      <div className="flex flex-wrap p-6">
+        <ButtonMain text="Play" icon="play.svg" />
+        <ButtonMain text="Practice" icon="guitar.svg" />
+        <ButtonMain text="Theory" icon="piano.svg" />
+        <ButtonMain text="Premium" icon="play.svg" />
       </div>
     </main>
   );
---
+++ spiral/components/Header.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header className="fixed left-0 right-0 top-0 bg-cyan-50 p-2 pl-6 flex flex-row items-center">
+      <button className="mr-4 p-1 rounded-md hover:bg-indigo-300 transition duration-500">
+        <Image alt="menu" src="/menu.svg" width={30} height={30} />
+      </button>
+      <button className="text-2xl font-extrabold text-cyan-900 hover:text-cyan-600 transition duration-500">
+        spiral
+      </button>
+      <button className="font-extralight text-2xl text-cyan-700 hover:text-cyan-600 tracking-wider hover:tracking-widest transition-all duration-1000 ease-out">
+        harmony
+      </button>
+      <button className="ml-auto mr-6 rounded-full bg-indigo-500 hover:bg-indigo-600 transition duration-500 ease-in-out text-white p-2 pl-3 pr-3 shadow-md">
+        LA
+      </button>
+    </header>
+  );
+}
