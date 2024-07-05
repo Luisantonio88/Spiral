72ffdec0c6dc908ccc1dd8e26e8a65b79ae4cea1
Luis Antonio Castro <luisantoniokoyari@gmail.com>
2024-07-05 14:03:29 +0200

    buttons

--- spiral/components/ButtonMain.tsx
+++ spiral/components/ButtonMain.tsx
@@ -8,7 +8,7 @@ export type ButtonProps = {
 export default function ButtonMain({ text, icon }: ButtonProps) {
   return (
     <button
-      className={`min-w-80 rounded-lg p-6 text-3xl bg-slate-50 hover:bg-cyan-50 active:bg-indigo-300 active:translate-y-2 transition duration-500 ease-in-out text-slate-400 hover:text-slate-500 m-3 shadow-md shadow-indigo-200 flex flex-col items-center justify-center hover:translate-y-1`}
+      className={`min-w-80 rounded-lg p-6 text-3xl bg-slate-50 hover:bg-indigo-200 active:bg-white active:translate-y-2 transition duration-500 ease-in-out text-slate-400 hover:text-indigo-500 m-3 shadow-md shadow-indigo-200 flex flex-col items-center justify-center hover:translate-y-1 hover:shadow-sm active:shadow-none`}
     >
       {text}
 
--- spiral/components/ButtonMainIndigo.tsx
+++ spiral/components/ButtonMainIndigo.tsx
@@ -8,7 +8,7 @@ export type ButtonProps = {
 export default function ButtonMainIndigo({ text, icon }: ButtonProps) {
   return (
     <button
-      className={`min-w-80 rounded-lg p-6 text-3xl bg-indigo-500 hover:bg-indigo-600 active:bg-slate-200 active:translate-y-2 transition duration-500 ease-in-out text-slate-50 m-3 shadow-md shadow-slate-400 flex flex-col items-center justify-center hover:translate-y-1`}
+      className={`min-w-80 rounded-lg p-6 text-3xl bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-800 active:translate-y-2 transition duration-500 ease-in-out text-slate-50 m-3 shadow-md shadow-slate-400 hover:shadow-sm active:shadow-none flex flex-col items-center justify-center hover:translate-y-1`}
     >
       {text}
 
