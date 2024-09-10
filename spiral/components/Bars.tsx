821787f6d138b4141a46a93372061a6ba622ade5
Luis Antonio Castro <luisantoniokoyari@gmail.com>
2024-09-11 01:26:59 +0200

    working drag

--- spiral/components/Bars.tsx
+++ spiral/components/Bars.tsx
@@ -1,15 +1,64 @@
-export default function Bars() {
+import React, { useState } from "react";
+
+interface Bar {
+  id: number;
+  chordName: string;
+  chordKey: string;
+  chordType: string;
+}
+
+interface BarsProps {
+  playChord: (chordKey: string, chordType: string) => void; // Add this line
+}
+
+export default function Bars({ playChord }: BarsProps) {
+  const [bars, setBars] = useState<Bar[]>([
+    { id: 1, chordName: "Drag", chordKey: "", chordType: "" },
+    { id: 2, chordName: "Drop", chordKey: "", chordType: "" },
+    { id: 3, chordName: "and", chordKey: "", chordType: "" },
+    { id: 4, chordName: "Play!", chordKey: "", chordType: "" },
+  ]);
+
+  const onDrop = (e: React.DragEvent<HTMLDivElement>, index: number) => {
+    const chord = e.dataTransfer.getData("chord");
+    const [chordKey, chordType] = chord.split(",");
+
+    const updatedBars = [...bars];
+    updatedBars[index] = {
+      ...updatedBars[index],
+      chordName: chordKey + (chordType ? chordType : ""),
+      chordKey,
+      chordType,
+    };
+    setBars(updatedBars);
+  };
+
+  const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
+  const handleBarClick = (chordKey: string, chordType: string) => {
+    if (chordKey && chordType) {
+      playChord(chordKey, chordType); // Play the chord when bar is clicked
+    }
+  };
+
   return (
-    <>
-      <div
-        id="playerContainer"
-        className="flex flex-row w-full mt-4 justify-center items-center gap-8 m-2"
-      >
-        <div className="bar 1">Drag,</div>
-        <div className="bar 2">Drop,</div>
-        <div className="bar 3">and</div>
-        <div className="bar 4">Play!</div>
-      </div>
-    </>
+    <div
+      id="playerContainer"
+      className="flex flex-row w-full mt-4 justify-center items-center gap-8 m-2"
+    >
+      {bars.map((bar, index) => (
+        <div
+          key={bar.id}
+          className="bar w-20 h-20 border text-center flex justify-center items-center"
+          onDrop={(e) => onDrop(e, index)}
+          onDragOver={onDragOver}
+          onClick={() => handleBarClick(bar.chordKey, bar.chordType)}
+        >
+          {bar.chordName}
+        </div>
+      ))}
+    </div>
   );
 }
--- spiral/components/ChordPlayer.tsx
+++ spiral/components/ChordPlayer.tsx
@@ -2,6 +2,7 @@
 
 import React, { useRef } from "react";
 import AudioPlayer from "./AudioPlayer";
+import { ChordType, chords } from "@/app/chords";
 
 interface ChordPlayerProps {
   instrument: string;
@@ -16,120 +17,6 @@ export default function ChordPlayer({
   setActiveKeys,
   chordType,
 }: ChordPlayerProps) {
-  type ChordType = "major" | "minor" | "dim" | "maj7" | "min7" | "7" | "min7b5";
-  type Chords = { [key: string]: { [key in ChordType]: string[] } };
-
-  const chords: Chords = {
-    C: {
-      major: ["C3", "E4", "G4", "C5"],
-      minor: ["C3", "Eb4", "G4", "C5"],
-      dim: ["C3", "Eb4", "Gb4", "C5"],
-      maj7: ["C3", "E4", "G4", "B4"],
-      min7: ["C3", "Eb4", "G4", "Bb4"],
-      7: ["C3", "E4", "G4", "Bb4"],
-      min7b5: ["C3", "Eb4", "Gb4", "Bb4"],
-    },
-    Db: {
-      major: ["Db3", "Db4", "F4", "Ab4"],
-      minor: ["Db3", "Db4", "F4", "Ab4"],
-      dim: ["Db3", "Db4", "E4", "G4"],
-      maj7: ["Db3", "F4", "Ab4", "C5"],
-      min7: ["Db3", "E4", "Ab4", "B4"],
-      7: ["Db3", "F4", "Ab4", "B4"],
-      min7b5: ["Db3", "E4", "G4", "B4"],
-    },
-    D: {
-      major: ["D3", "D4", "Gb4", "A4"],
-      minor: ["D3", "D4", "F4", "A4"],
-      dim: ["D3", "D4", "F4", "Ab4"],
-      maj7: ["D3", "Gb4", "A4", "Db5"],
-      min7: ["D3", "F4", "A4", "C5"],
-      7: ["D3", "Gb4", "A4", "C5"],
-      min7b5: ["D3", "F4", "Ab4", "C5"],
-    },
-    Eb: {
-      major: ["Eb3", "Eb4", "G4", "Bb4"],
-      minor: ["Eb3", "Eb4", "Gb4", "Bb4"],
-      dim: ["Eb3", "Eb4", "Gb4", "A4"],
-      maj7: ["Eb3", "D4", "G4", "Bb4"],
-      min7: ["Eb3", "Db4", "Gb4", "Bb4"],
-      7: ["Eb3", "Db4", "G4", "Bb4"],
-      min7b5: ["Eb3", "Db4", "Gb4", "A4"],
-    },
-    E: {
-      major: ["E3", "E4", "Ab4", "B4"],
-      minor: ["E3", "E4", "G4", "B4"],
-      dim: ["E3", "E4", "G4", "Bb4"],
-      maj7: ["E3", "D4", "Ab4", "B4"],
-      min7: ["E3", "D4", "G4", "B4"],
-      7: ["E3", "D4", "Ab4", "B4"],
-      min7b5: ["E3", "D4", "G4", "Bb4"],
-    },
-    F: {
-      major: ["F3", "F4", "A4", "C5"],
-      minor: ["F3", "F4", "Ab4", "C5"],
-      dim: ["F3", "F4", "Ab4", "B4"],
-      maj7: ["F3", "E4", "A4", "C5"],
-      min7: ["F3", "Eb4", "Ab4", "C5"],
-      7: ["F3", "Eb4", "A4", "C5"],
-      min7b5: ["F3", "Eb4", "Ab4", "B4"],
-    },
-    Gb: {
-      major: ["Gb3", "Db4", "Gb4", "Bb4"],
-      minor: ["Gb3", "Db4", "Gb4", "A4"],
-      dim: ["C3", "C4", "Gb4", "A4"],
-      maj7: ["Gb3", "Db4", "F4", "Bb4"],
-      min7: ["Gb3", "Db4", "E4", "A4"],
-      7: ["Gb3", "Db4", "E4", "Bb4"],
-      min7b5: ["Gb3", "C4", "E4", "A4"],
-    },
-    G: {
-      major: ["G3", "D4", "G4", "B4"],
-      minor: ["G3", "D4", "G4", "Bb4"],
-      dim: ["G3", "Db4", "G4", "Bb4"],
-      maj7: ["G3", "D4", "Gb4", "B4"],
-      min7: ["G3", "D4", "F4", "Bb4"],
-      7: ["G3", "D4", "F4", "B4"],
-      min7b5: ["G3", "Db4", "F4", "Bb4"],
-    },
-    Ab: {
-      major: ["Ab3", "Eb4", "Ab4", "C5"],
-      minor: ["Ab3", "Eb4", "Ab4", "B4"],
-      dim: ["Ab3", "D4", "Ab4", "B4"],
-      maj7: ["Ab3", "Eb4", "G4", "C5"],
-      min7: ["Ab3", "Eb4", "Gb4", "B4"],
-      7: ["Ab3", "Eb4", "Gb4", "C5"],
-      min7b5: ["Ab3", "D4", "Gb4", "B4"],
-    },
-    A: {
-      major: ["A3", "E4", "A4", "Db5"],
-      minor: ["A3", "E4", "A4", "C5"],
-      dim: ["A3", "Eb4", "A4", "C5"],
-      maj7: ["A3", "E4", "Ab4", "Db5"],
-      min7: ["A3", "E4", "G4", "C5"],
-      7: ["A3", "E4", "G4", "Db5"],
-      min7b5: ["A3", "Eb4", "G4", "C5"],
-    },
-    Bb: {
-      major: ["Bb3", "D4", "F4", "Bb4"],
-      minor: ["Bb3", "Db4", "F4", "Bb4"],
-      dim: ["Bb3", "Db4", "E4", "Bb4"],
-      maj7: ["Bb3", "F4", "A4", "D5"],
-      min7: ["Bb3", "F4", "Ab4", "Db5"],
-      7: ["Bb3", "F4", "Ab4", "D5"],
-      min7b5: ["Bb3", "E4", "Ab4", "Db5"],
-    },
-    B: {
-      major: ["B3", "Eb4", "Gb4", "B4"],
-      minor: ["B3", "D4", "Gb4", "B4"],
-      dim: ["B3", "D4", "F4", "B4"],
-      maj7: ["B3", "Eb4", "Gb4", "Bb4"],
-      min7: ["B3", "D4", "Gb4", "A4"],
-      7: ["B3", "Eb4", "Gb4", "A4"],
-      min7b5: ["B3", "D4", "F4", "A4"],
-    },
-  };
-
   const audioPlayerRef = useRef<{
     playSound: (notes: string | string[]) => void;
   } | null>(null);
@@ -141,6 +28,14 @@ export default function ChordPlayer({
       setActiveKeys(notes);
       setTimeout(() => setActiveKeys([]), 500); // Reset active keys after some time
     }
+  };
+
+  const onDragStart = (
+    e: React.DragEvent<HTMLButtonElement>,
+    chordKey: string,
+    chordType: ChordType
+  ) => {
+    e.dataTransfer.setData("chord", `${chordKey},${chordType}`);
   };
 
   return (
@@ -155,12 +50,16 @@ export default function ChordPlayer({
           <div className="chord-key Gb">
             <button
               className="chord-button III"
+              draggable
+              onDragStart={(e) => onDragStart(e, "Bb", "minor")}
               onClick={() => playChord("Bb", "minor")}
             >
               Bb-
             </button>
             <button
               className="chord-button II"
+              draggable
+              onDragStart={(e) => onDragStart(e, "Ab", "minor")}
               onClick={() => playChord("Ab", "minor")}
             >
               Ab-
@@ -731,12 +630,16 @@ export default function ChordPlayer({
           <div className="chord-key Gb">
             <button
               className="chord-button III"
+              draggable
+              onDragStart={(e) => onDragStart(e, "Bb", "min7")}
               onClick={() => playChord("Bb", "min7")}
             >
               Bb-7
             </button>
             <button
               className="chord-button II"
+              draggable
+              onDragStart={(e) => onDragStart(e, "Ab", "min7")}
               onClick={() => playChord("Ab", "min7")}
             >
               Ab-7
